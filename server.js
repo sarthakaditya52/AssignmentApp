f8c0423c575eff54840e7deb6341df990184f2c5
sarthakaditya52 <sarthak.aditya52@gmail.com>
2020-03-23 16:28:48 +0530

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path'); 
+
+const config = require('config');
+
 const app = express();
 
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
+// DB Config
+const db = config.get('mongoURI');
+
+//Connect to Mongo
+mongoose
+    .connect(db, { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true    
+    })
+    .then(() => console.log('MongoDB Connected....'))
+    .catch(err => console.log(err));
+
+    
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
