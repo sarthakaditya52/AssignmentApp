39b2fd09c6e333c53f71a776073e7ad1f498f9aa
sarthakaditya52 <sarthak.aditya52@gmail.com>
2020-03-24 02:09:37 +0530

    Create Assignments

--- model/Assignment.js
+++ model/Assignment.js
@@ -14,6 +14,6 @@ const AssignSchema = new Schema({
         type: String,
         required: true
     }
-})
+});
 
 module.exports = Assignment = mongoose.model('assignment', AssignSchema);
--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const config = require('config');
+const bodyParser = require('body-parser');
 
-// User Model
+router.use(bodyParser.urlencoded({ extended: false}));
+router.use(bodyParser.json());
+
+// Assignment Model
 const Assignment = require('../../model/Assignment');
 
-router.get('/', (req, res) => {
-    res.render('index',{
-        title: 'working'
-    })
-})
-
+// @route ->-> Get api/admin/createAssignments  
+// @description ->-> Create Assignment Page
+// @access ->->Public
 router.get('/createAssignments', (req, res) => {
-    res.render('addAssignment')
-})
+    res.render('addAssignment');
+});
+
+// @route ->-> POST api/admin/createAssignments  
+// @description ->-> Put Assignments 
+// @access ->->Public
+router.post('/createAssignments', (req,res) => {
+    assignment = new Assignment;
+    assignment.name = req.body.name;
+    assignment.task = req.body.task;
+    assignment.day = req.body.day;
+
+    assignment.save( function(err) {
+        if(err)
+            {
+                console.log(err);
+                return;
+            }
+        else
+            res.render('index', {
+                title: 'Assignment Added'
+            });
+    });
+    return
+});
+
 
 module.exports = router;
